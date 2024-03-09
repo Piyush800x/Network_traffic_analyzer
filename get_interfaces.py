a043cca532c5b8290a30110913a7e2c6f43293c6
shibammitra24 <shibammitra695@gmail.com>
2024-03-09 19:03:37 +0530

    base project added

---
+++ get_interfaces.py
@@ -0,0 +1,4 @@
+from scapy.arch import get_windows_if_list
+
+interfaces = get_windows_if_list()
+print(interfaces)
---
+++ main.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+
+def analyze_packet(packet):
+    if IP in packet:
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        print(f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+
+def capture_traffic(interface="Wi-Fi"):
+    print(f"[*] Starting capture on interface {interface}")
+    sniff(iface=interface, prn=analyze_packet, store=0)
+
+if __name__ == "__main__":
+    capture_traffic()
